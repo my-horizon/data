644c99421146dbe1247eb9b10b54542d3933e5cf
my-horizon <mail@virtualhorizon.de>
2025-12-09 21:33:58 +0100

    continued with c:memory

--- projects/c/tutorials/memory.c
+++ projects/c/tutorials/memory.c
@@ -56,9 +56,52 @@ int main() {
   // 'ptr[3]' is not specified, so a 0 will be printed because of 'calloc' initialising the memory to 0!
 
   printf("%d\n", *ptr);
-  printf("%d %d %d", ptr[1], ptr[2], ptr[3]);
+  printf("%d %d %d\n", ptr[1], ptr[2], ptr[3]);
 
-  // continue with c:access memory
+  // dynamic memory does not have its own data type, the data in the memory can be interpreted as a type based on the data type of the pointer
+  // a pointer to 4 bytes can be interpreted as one 'int' value (4 bytes) or as an array of 4 'char' values (1 byte each)
+  int *ptr3 = malloc(4);
+  char *ptr4 = (char *)ptr3;
+  ptr3[0] = 1684234849;
+
+  printf("%d is %c %c %c %c\n", *ptr3, ptr4[0], ptr4[1], ptr4[2], ptr4[3]);
+
+  // if the reserved amount of memory is not enough, it can be reallocated to make it larger
+  // reallocation reserves a different (usually larger) amount of memory while keeping the data that was stored in it
+  // use the 'realloc()' function to change the size
+  // int *ptrN = realloc(ptr1, size);
+  int *ptr01, *ptr02, size;
+
+  size = 4 * sizeof(*ptr01); // allocate memory for 4 integers
+  ptr01 = malloc(size);
+
+  printf("%d bytes allocated at address %p\n", size, ptr01);
+
+  size = 6 * sizeof(*ptr01); // resize the memory to hold 6 integers
+  ptr02 = realloc(ptr01, size);
+
+  printf("%d bytes reallocated at address %p\n", size, ptr02);
+
+  // the 'realloc()' function returns a 'NULL' pointer if it is not able to allocate more memory
+  // this example checks whether 'realloc()' is able to resize the memory or not, by checking for a 'NULL' pointer
+  int *ptr03, *ptr04;
+
+  ptr03 = malloc(4); // allocate memory
+
+  ptr04 = realloc(ptr03, 8); // attempt to resize the memory
+
+  // check whether realloc is able to resize or not
+  // error checking should always be included when when allocating memory
+  if (ptr04 == NULL) {
+    printf("Failed. Unable to resize memory!\n");
+  } else {
+    printf("Success. 8 bytes reallocated at address %p!\n", ptr04);
+  }
+
+  // the memory should always be "freed"/released after usage (important to make sure program is behaving as expected)
+  free(ptr03);
 
   return 0;
 }
+
+// continue with c:deallocate memory
